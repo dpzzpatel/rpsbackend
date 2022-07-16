7f998acc8f4a07206b39643ad9cd4230f8250e1f
dpzzpatel <dpzzpatel@gmail.com>
2022-07-16 16:33:00 +0530

    changed server config

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http,{
 	cors: {
-    origin: 'http:localhost:4001,rps-mocha.vercel.app,rps-dpzzpatel.vercel.app',
+    origin: 'http:localhost:4001,https://rps-mocha.vercel.app/,https://rps-mocha.vercel.app/',
   }
 });
 const dotenv = require('dotenv');
